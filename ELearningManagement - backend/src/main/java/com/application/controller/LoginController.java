d3568c2f071f930b13e7ec7478a1dd4bdf0484e5
Chedysk <chedy.sakly@gmail.com>
2024-03-28 22:57:59 +0100

    environmental variables

--- ELearningManagement - backend/src/main/java/com/application/controller/LoginController.java
+++ ELearningManagement - backend/src/main/java/com/application/controller/LoginController.java
@@ -24,7 +24,7 @@ public class LoginController
     }
 	
 	@PostMapping("/loginuser")
-	@CrossOrigin(origins = "http://frontend-service:30001")
+	@CrossOrigin(origins = "http://192.168.49.2:30001")
 	public User loginUser(@RequestBody User user) throws Exception
 	{
 		String currEmail = user.getEmail();
@@ -43,7 +43,7 @@ public class LoginController
 	}
 
 	@PostMapping("/loginprofessor")
-	@CrossOrigin(origins = "http://frontend-service:30001")
+	@CrossOrigin(origins = "http://192.168.49.2:30001")
 	public Professor loginDoctor(@RequestBody Professor professor) throws Exception
 	{
 		String currEmail = professor.getEmail();
